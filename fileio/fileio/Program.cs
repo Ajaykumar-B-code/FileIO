4ab50e3f199bb2866078a02cddd4aff156dec22d
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-02-09 15:03:14 +0530

    [Ajay] Add: Read data from the file using inputStream

---
+++ fileio/fileio/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace fileio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data;
+            StreamReader reader = new StreamReader("C:\\Users\\91767\\Desktop\\workspace\\Fileio\\text.txt");
+            data = reader.ReadLine();
+
+            while (data != null)
+            {
+                Console.WriteLine(data); 
+                data = reader.ReadLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
