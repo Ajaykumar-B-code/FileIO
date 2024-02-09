1f1cc105741f81d84d557b7cc816bbadc42c967c
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-02-09 15:33:15 +0530

    [Ajay] Add: write data in the file

--- fileio/fileio/Program.cs
+++ fileio/fileio/Program.cs
@@ -11,15 +11,25 @@ namespace fileio
     {
         static void Main(string[] args)
         {
-            string data;
-            StreamReader reader = new StreamReader("C:\\Users\\91767\\Desktop\\workspace\\Fileio\\text.txt");
-            data = reader.ReadLine();
+            //string data;
+            //StreamReader reader = new StreamReader("C:\\Users\\91767\\Desktop\\workspace\\Fileio\\text.txt");
+            //data = reader.ReadToEnd();
 
-            while (data != null)
+            //Console.WriteLine(data);
+            //reader.Close(); // to write in the same we should close the file first
+            StreamWriter write = new StreamWriter("C:\\Users\\91767\\Desktop\\workspace\\Fileio\\text.txt");
+            write.WriteLine("natural numbers from 1-100");
+            for(int i = 0; i < 100; i++)
             {
-                Console.WriteLine(data); 
-                data = reader.ReadLine();
+                write.WriteLine(i);
             }
+            write.Close();  // compulsory close the file
+            //while (data != null)
+            //while (data != null)
+            //{
+            //    Console.WriteLine(data); 
+            //    data = reader.ReadLine();
+            //}
             Console.ReadLine();
         }
     }
