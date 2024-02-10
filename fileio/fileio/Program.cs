039dbd41b066e8cd2042e2997221fc2ea3ca27ad
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-02-10 10:35:27 +0530

    [Ajay] Add: different methods to read/write data in file

--- fileio/fileio/Program.cs
+++ fileio/fileio/Program.cs
@@ -17,18 +17,52 @@ namespace fileio
 
             //Console.WriteLine(data);
             //reader.Close(); // to write in the same we should close the file first
-            StreamWriter write = new StreamWriter("C:\\Users\\91767\\Desktop\\workspace\\Fileio\\text.txt");
-            write.WriteLine("natural numbers from 1-100");
-            for(int i = 0; i < 100; i++)
-            {
-                write.WriteLine(i);
-            }
-            write.Close();  // compulsory close the file
+            //StreamWriter write = new StreamWriter("C:\\Users\\91767\\Desktop\\workspace\\Fileio\\text.txt");
+            //write.WriteLine("natural numbers from 1-100");
+            //for(int i = 0; i < 100; i++)
+            //{
+            //    write.WriteLine(i);
+            //}
+            //write.Close();  
+            // compulsory close the file
+
             //while (data != null)
             //while (data != null)
             //{
             //    Console.WriteLine(data); 
             //    data = reader.ReadLine();
+            //}
+
+            string fpath = @"C:\\Users\\91767\\Desktop\\workspace\\Fileio\\text.txt";
+            //FileInfo f1 = new FileInfo(fpath);
+            //Console.WriteLine(f1.FullName);
+            //Console.WriteLine(f1.Name);
+            //Console.WriteLine(f1.DirectoryName);
+            //Console.WriteLine(f1.Extension);
+            //Console.WriteLine(f1.Length.ToString());
+            //Console.WriteLine(f1.CreationTime);
+            File.WriteAllText(fpath, "To write in one line of the file");
+
+            string[] lin = new string[]
+            {
+                "to write in multi lines",
+                "we should create the array of string "
+            };
+
+
+            File.AppendAllLines(fpath, lin);
+
+            //if (File.Exists(fpath))
+            //{
+            //    string[] s = File.ReadAllLines(fpath);
+            //   foreach (var line in s)
+            //    {
+            //        Console.WriteLine(line);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("file doesnt exist");
             //}
             Console.ReadLine();
         }
